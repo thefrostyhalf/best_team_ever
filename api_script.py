2726aa38b998fd79cf5777a22391869f98b451a9
Chelan Patton <nalehc@gmail.com>
2018-03-01 23:38:19 +0000

    Add complete API url, but need to fix JSON

--- api_script.py
+++ api_script.py
@@ -10,6 +10,11 @@ tracks_sample_list = [{"artist": "Lady Gaga", "title": "Bad Romance"}]
 top_track_info = []
 
 for track in tracks_sample_list:
-        url = "https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=jsonp&callback=callback&q_track=" + track['artist'] + "&q_artist=" + track['title'] + '&apikey=2ad42aad9e1f1628b2418c17e486f84b'
-        print (url)
-
+        url = "http://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=jsonp&callback=callback&q_track=" + track['artist'] + "&q_artist=" + track['title'] + '&apikey=2ad42aad9e1f1628b2418c17e486f84b'
+        response = req.get(url).content
+        info = response.decode("utf-8")
+        data = json.loads(info)
+        #s = json.dumps(data, indent=4, sort_keys=True)
+        #print(type(info))
+        #top_track_info.append (response_json['callback']['body']['lyrics']['lyrics_body'])
+        #print (top_track_info)
