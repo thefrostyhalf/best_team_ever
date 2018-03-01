019b48be43fcb95be54aaa40e6bddd6c11eb5987
Chelan Patton <nalehc@gmail.com>
2018-03-01 21:59:22 +0000

    Add API call

--- api_script.py
+++ api_script.py
@@ -10,5 +10,6 @@ tracks_sample_list = [{"artist": "Lady Gaga", "title": "Bad Romance"}]
 top_track_info = []
 
 for track in tracks_sample_list:
-    for key, value in track.items(): 
-        print (value)
+        url = "https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=jsonp&callback=callback&q_track=" + track['artist'] + "&q_artist=" + track['title'] + '&apikey=2ad42aad9e1f1628b2418c17e486f84b'
+        print (url)
+
