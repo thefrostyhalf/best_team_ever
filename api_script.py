f4fe11d5c8fcb2372fe640b1982ea71526c94f17
Chelan Patton <nalehc@gmail.com>
2018-03-01 19:19:49 -0800

    Working lyric output using sample data

--- api_script.py
+++ api_script.py
@@ -3,18 +3,16 @@ import json
 import requests as req
 import numpy as np
 import matplotlib.pyplot as plt
+import re
 
-tracks_sample_list = [{"artist": "Lady Gaga", "title": "Bad Romance"}]
+tracks_sample_list = [{"artist": "Lady Gaga", "title": "Bad Romance", "genre": "pop"}]
 
 #Dicts to collect API data
 top_track_info = []
 
 for track in tracks_sample_list:
-        url = "http://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=jsonp&callback=callback&q_track=" + track['artist'] + "&q_artist=" + track['title'] + '&apikey=2ad42aad9e1f1628b2418c17e486f84b'
-        response = req.get(url).content
-        info = response.decode("utf-8")
-        data = json.loads(info)
-        #s = json.dumps(data, indent=4, sort_keys=True)
-        #print(type(info))
-        #top_track_info.append (response_json['callback']['body']['lyrics']['lyrics_body'])
-        #print (top_track_info)
+        url = "https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=json&q_track=" + track['artist'] + "&q_artist=" + track['title'] + '&apikey=2ad42aad9e1f1628b2418c17e486f84b'
+        response_json = req.get(url).json()
+        lyrics = (response_json['message']['body']['lyrics']['lyrics_body'])
+        top_track_info.append (response_json['message']['body']['lyrics']['lyrics_body'].replace("\n", ''))
+        print (top_track_info)
