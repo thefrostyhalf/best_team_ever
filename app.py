34e75a84a66573849c1c1242d2e250795fdafcfb
Chelan Patton <nalehc@gmail.com>
2018-03-06 21:37:02 -0800

    Working dropdown

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import requests
 import pymongo
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 conn = "mongodb://localhost:27017"
@@ -9,17 +9,30 @@ db = client.songDB
 
 @app.route("/")
 def index():
-    song_data =  list(db.song_data.find())
     return render_template("index.html")
 
-@app.route("/genre_song")
-def reference():
-    song_data = list(db.song_data.find())
+@app.route("/song_data")
+def songs_genes():
+    song_list =  (db.song_data.find(projection = { 'title': True, 'genre': True }))
+    options = []
+    for song in song_list:
+        options.append(song['title'])
+        if song['genre'] not in options:
+            options.append(song['genre'])
+    options.sort()
+    return jsonify(options)
+
+
+@app.route("/genre_song.html")
+def genre_song():
     return render_template("genre_song.html")
 
-@app.route("/word_analysis")
-def reference():
-    song_data = list(db.song_data.find())
+@app.route("/sentiment.html")
+def sentiment():
+    return render_template("sentiment.html")
+
+@app.route("/word_analysis.html")
+def word_analysis():
     return render_template("word_analysis.html")
 
 if __name__ == "__main__":
